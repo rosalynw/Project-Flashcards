3a7b23c7813fbc9a976f2c1bde5f52862f954b7c
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-05-22 20:37:37 -0400

    init commit

---
+++ src/Cards/AddCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddCard ({decks}) {
+    return (
+        <div>
+            <nav>Home / / Add Card</nav>
+            <h5>Name: Add Card</h5>
+        </div>
+    )
+}
+
+export default AddCard;
