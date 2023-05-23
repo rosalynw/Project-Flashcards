f9a84f66e2f6c0aa776b1753b47bcf023bfca245
Rosalyn Wimberly <rosalynwimberly@gmail.com>
2023-05-22 21:24:00 -0400

    restarted from original project code

---
+++ src/Layout/home/Home.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import {listDecks } from "../../utils/api"
+import {Link} from "react-router-dom"
+import DeckList from "../Deck/DeckList";
+
+function Home() {
+    //should have buttons and list of decks
+    const [decks, setDecks] = useState([]);
+
+    useEffect(() => {
+        async function fetchDecks(){
+            const response = await listDecks();
+            setDecks(response);
+        }
+        fetchDecks();
+    }, [])
+
+    return (
+        <div>
+            <Link >Create Deck</Link>
+            <DeckList decks={decks} />
+        </div>
+    )
+}
+
+export default Home;
--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,50 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import NotFound from "./NotFound";
-import Home from "../Cards/Home";
 import {Switch, Route} from "react-router-dom"
-import Study from "../Cards/Study";
-import { listDecks } from "../utils/api";
-import CreateDeck from "../Cards/CreateDeck";
-import DeckList from "../Cards/DeckList";
-import AddCard from "../Cards/AddCard"
+import Home from "./home/Home";
+
 
 function Layout() {
-  const [decks, setDecks] = useState([]);
-
-       useEffect(() => {
-        async function fetchDecks() {
-            try {
-              const fetchedDecks  = await listDecks();
-              setDecks(fetchedDecks);
-              
-            } catch (error) {
-              console.error(error);
-            }
-          }
-          fetchDecks();
-    }, [])
 
   return (
     <>
       <Header />
       <div className="container">
-        <Switch>
+      <Switch>
         {/* TODO: Implement the screen starting here */}
         <Route exact path="/">
-          <Home decks={decks} />
+          <Home />
         </Route>
         <Route path="/decks/:deckId/study">
-          <Study decks={decks}/>
+          {/* <Study /> */}
         </Route>
         <Route path="/decks/new">
-          <CreateDeck decks={decks} />
+          {/* <CreateDeck /> */}
         </Route>
         <Route path="/decks/:deckId">
-          <DeckList decks={decks} />
+          {/* <DeckList /> */}
         </Route>
         <Route>
-          <AddCard />
+          {/* <AddCard /> */}
         </Route>
         <Route >
           <NotFound />
